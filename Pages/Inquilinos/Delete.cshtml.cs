f1d818e5be2641a9cceb05d5ba0a0d608286befa
Juand7M <trabajosfcecep4@gmail.com>
2024-11-23 18:29:41 -0500

    Delete Inquilinos

---
+++ Pages/Inquilinos/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PropiedadesWEB.Data;
+using PropiedadesWEB.Models;
+
+namespace _PropertyManager.Pages.Inquilinos
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly PropiedadesContext _context;
+
+        public DeleteModel(PropiedadesContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Inquilino Inquilino { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Inquilinos == null)
+            {
+                return NotFound();
+            }
+
+            var inquilino = await _context.Inquilinos.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (inquilino != null)
+            {
+                Inquilino = inquilino;
+                _context.Inquilinos.Remove(inquilino);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
